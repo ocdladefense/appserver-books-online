615693de0eb0ec798a447c5756daca6b6f6b9b6b
Jose Bernal <jbernal.web.dev@gmail.com>
2022-02-27 14:36:25 -0800

    First commit.

--- module.php
+++ module.php
@@ -1,9 +1,111 @@
 <?php
 
-class ModuleTemplateModule extends Module {
+use Mysql\Database;
+use Http\HttpRequest;
+use Http\HttpHeader;
+use Mysql\DbHelper;
+use Mysql\QueryBuilder;
+use Http\HttpHeaderCollection;
+
+use function Mysql\insert;
+use function Mysql\update;
+use function Mysql\select;
+use function Session\get_current_user;
+
+
+
+
+
+class BonModule extends Module {
 
     public function __construct() {
 
         parent::__construct();
     }
+
+
+
+    public function getRecentCarList($court = 'Oregon Appellate Court', DateTime $begin = null, DateTime $end = null) {
+		$begin = null == $begin ? new DateTime() : $begin;
+		
+		$beginMysql = $begin->format('Y-m-j');
+
+		if(null == $end) {
+			$query = "SELECT * FROM car WHERE decision_date = '{$beginMysql}'";
+			$query .= " AND court = '{$court}'";
+		} else {
+			$endMysql = $end->format('Y-m-j');
+	
+			$query = "SELECT * FROM car WHERE decision_date >= '{$beginMysql}'";
+			$query .= " AND decision_date <= '{$endMysql}'";
+			$query .= " AND court = '{$court}'";
+		}
+
+
+
+		// print $query;exit;
+		// ORDER BY year DESC, month DESC, day DESC";
+		$cars = select($query);
+		
+		// var_dump($cars);exit;
+
+		$list = new Template("email-list");
+		$list->addPath(__DIR__ . "/templates");
+
+		$listHtml = $list->render(["cars" => $cars]);
+
+		$body = new Template("email-body");
+		$body->addPath(__DIR__ . "/templates");
+
+		$params = [
+			"year" => $begin->format('Y'),
+			"month" => $begin->format('m'),
+			"day" => $begin->format('j'),
+			"date" => $begin->format('l, M j  Y'),
+			"carList" => $listHtml 
+		];
+
+	
+		return $body->render($params);
+	}
+
+
+
+
+
+	public function doMail($to, $subject, $content, $headers = array()){
+
+		$headers = [
+			"From" 		   => "notifications@ocdla.org",
+			"Content-Type" => "text/html"
+		];
+
+		$headers = HttpHeaderCollection::fromArray($headers);
+
+
+
+		$message = new MailMessage($to);
+		$message->setSubject($subject);
+		$message->setBody($content);
+		$message->setHeaders($headers);
+
+		return $message;
+	}
+
+
+
+	public function testMail() {
+
+
+		$to = "jbernal.web.dev@gmail.com";
+		$subject = "Books Online notifications";
+
+
+		$range = new DateTime("2022-1-10");
+		$end = new DateTime();
+		$content = "My sample content.";
+		
+
+		return $this->doMail($to, $subject, $content);
+	}
 }
---
+++ templates/expiring-first-notification.tpl.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Template to render a Books Online notification (first notice);
+
+*/
+?>
+
+<p>Dear {!Contact.FirstName},</p>
+
+<p>Your OCDLA Books Online Subscription year is due to renew on {!OrderItem.ExpirationDate__c}. For only $149, you have a full twelve months’ access to OCDLA’s incredible research manuals such as Felony Sentencing in Oregon, the DUII Notebook, Search & Seizure Manual, Defending Sex Cases, and OCDLA’s veterans’ representation manual edited by Jesse Wm. Barton, Still At War. Renew today.</p>
+
+ 
+
+<h3>Major Advantage–You’re the First to Receive Updates!</h3>
+<p>As a Books Online subscriber, you have instant, early access to the updates to Felony Sentencing and Search & Seizure — these updates are inserted exactly where they belong in the chapters. **Brand New: the 2018 updates to Search & Seizure in Oregon are in! What's more, the updates are added online before the PDF or hard copy formats—a major advantage to you as a Books Online subscriber.</p>
+
+ 
+
+<h3>Access Is Easier Than Ever</h3>
+<p>We’ve now made accessing Books Online easier than ever with a new, easily recognized web menu icon in the upper left corner of the Library of Defense. Click the three-line icon and open up a “drawer” housing the eight manuals:</p>
+
+
+<img alt="Books Online drawer" src="/modules/bon/images/email--bon-arrow.png" />
+<br />
+<img alt="Books Online drawer" src="email--iphone-bon-1.png" />
+
+
+<p>Click one of the manuals and each chapter appears:
+<br />
+
+
+<img alt="Books Online drawer" src="email--iphone-bon-2.png" />
+</p>
+
+
+
+<p>Conduct your searches faster than ever with this new feature to the Library of Defense.</p>
+
+
+
+<h3>Improved Readability</h3>
+<p>All chapters have been reformatted to enhance readability—we’ve added section headers and an internal Table of Contents. Motions are accessible at the top of the page.</p>
+
+<h3>Time to Renew!</h3>
+<p>Renew before your subscription ends on {!OrderItem.ExpirationDate__c} and give yourself the peace of mind that you’ll have access to this material when you need it.</p>
+
+<p>Sincerely,</p>
+
+<p>Shaun S. McCrea<br />
+Executive Director</p>
